2f9b5c1ba5f474b28b4a1c5eb917dc7f7f47a934
Natacha Romano <natacharomanonr@gmail.com>
2020-07-19 20:11:03 -0300

    background image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Section from '../Section/Section';
+import './App.scss'
+
+import bgImg from '../../assets/images/bg-intro-desktop.svg'
+import bgImgMb from '../../assets/images/bg-intro-mobile.svg'
+import bgImg2 from '../../assets/images/image-mockups.png'
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Section seClass="a">
-        oiiiii
+      <div className="content">
+        <Section seClass="a">
+          <div className="container">
+            <div>
+              <img className="imgBg mb_hide" src={bgImg} />
+              <img className="imgBgMb tb_hide dt_hide" src={bgImgMb} />
+              <img className="imgBg2" src={bgImg2} />
+            </div>
+            <div>text here</div>
+          </div>
+        </Section>
+        <Section seClass="b">
+          <div className="container">
+            sdswdd
+        </div>
+        </Section>
+        <Section seClass="a">
+          alo
       </Section>
-      <Section seClass="b">
-        sdswdd
-      </Section>
-      <Section seClass="a">
-        alo
-      </Section>
+      </div>
     </div>
   );
 }
